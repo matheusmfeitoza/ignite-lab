9228c6b67f6170774e2f59f8391ae2c0565d1ac6
Matheus Marinho <feitozamatheus123@gmail.com>
2022-06-23 12:41:34 -0300

    feat: Added query to claim API data

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,21 +3,20 @@ interface ButtonProps {
     variant: "primary" | "secundary" | "default";
     text: string;
     iconSize: number;
-    slug: string;
 
 }
 export function Button(props: ButtonProps) {
     return (
-        props.variant === "primary" ? (<a href={props.slug} className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+        props.variant === "primary" ? (<a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
         <DiscordLogo size={props.iconSize}/>
         {props.text}
       </a>)
       : props.variant === "secundary" ? (
-        <a href={props.slug} className="p-4 text-sm text-blue-500 border border-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
+        <a href="#" className="p-4 text-sm text-blue-500 border border-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
         <Lightning size={props.iconSize}/>
         {props.text}
       </a>)
-      : props.variant === 'default' ? (<a href={props.slug} className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+      : props.variant === 'default' ? (<a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
       <Info size={props.iconSize}/>
       {props.text}
     </a>)
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: "https://api-sa-east-1.graphcms.com/v2/cl4o6kqhq0wwm01z2gttd7kxw/master",
-    cache: new InMemoryCache()
-})
+  uri: "https://api-sa-east-1.graphcms.com/v2/cl4o6kqhq0wwm01z2gttd7kxw/master",
+  cache: new InMemoryCache(),
+});
